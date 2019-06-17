91172981b36c064fbf8b80c424cb19cd22179296
Jackwright102557058 <48079170+Jackwright102557058@users.noreply.github.com>
2019-06-17 13:50:08 +1000

    practice

---
+++ Term_2/Assessment Practice/quiz.js
@@ -0,0 +1,26 @@
+var uinput1 = document.getElementById("uinput1").value;
+var uinput2 = document.getElementById("uinput2").value;
+var uinput3 = document.getElementById("uinput3").value;
+var subbutton = document.getElementById("submit");
+var bool1 = "no"
+var bool2 = "no"
+var bool3 = "no"
+
+subbutton.addEventListener("click", subbuttonclick);
+
+if (intput1 == "D")
+{
+    bool1 = "yes";
+}
+
+
+
+
+
+function subbuttonclick() {
+
+    if (bool1 /*&& bool2 && bool3*/ == yes)
+    {
+    alert("You Scored 3/3!!");
+    }
+}
